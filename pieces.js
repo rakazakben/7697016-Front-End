74b8d28cbc8b125d5e076058170e352d77d24b04
rakazak <rakazakben@gmail.com>
2024-12-24 11:16:24 +0100

    test node

--- pieces.js
+++ pieces.js
@@ -3,39 +3,67 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const  prixElement = document.createElement("p");
-prixElement.innerText = `Prix : ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
+//const article = pieces[0];
+for(let i=0; i < pieces.length ; i++){
 
-/*
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('pieces-autos.json');
-const pieces = await reponse.json();
-// Création des balises 
-const article = pieces[0];
+const pieceElement = document.createElement("article");
 const imageElement = document.createElement("img");
-imageElement.src = article.image;
+imageElement.src = pieces[i].image;
 const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Priiiiix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+nomElement.innerText = pieces[i].nom;
+const  prixElement = document.createElement("p");
+prixElement.innerText = `Prix : ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-//Rattachement de nos balises au DOM
+categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = pieces[i].description ?? "(pas de description pour le moment)" ;
+const stockElement = document.createElement("p");
+stockElement.innerText = `stock : ${pieces[i].disponibilite === true ? "en stock" : "rupture de stock"}`;
+
 const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-*/
+sectionFiches.appendChild(pieceElement);
+pieceElement.appendChild(imageElement);
+pieceElement.appendChild(nomElement);
+pieceElement.appendChild(descriptionElement);
+pieceElement.appendChild(prixElement);
+pieceElement.appendChild(categorieElement);
+pieceElement.appendChild(stockElement);
+}
+//boutons de tri
+const boutonTrier = document.querySelector(".btn-trier");
+boutonTrier.addEventListener("click", function () {
+    const listeOrdonnee = Array.from(pieces);
+    listeOrdonnee.sort(function(a,b){
+        return a.prix - b.prix ;
+    });
+    console.log(listeOrdonnee);
+});
+
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function() {
+
+    const pieceFiltre = pieces.filter(function(piece){
+        return piece.prix <= 35;
+    });
+    console.log(pieceFiltre);
+});
+
+const boutonDescription = document.querySelector(".btn-description");
+
+boutonDescription.addEventListener("click", function(){
+    const pieceDescription = pieces.filter(function(piece){
+        return piece.description ;
+    });
+    console.log(pieceDescription);
+});
+
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function(){
+    const listeOrdonnee = Array.from(pieces);
+    listeOrdonnee.sort(function(a,b){
+        return b.prix - a.prix ;
+    });
+    console.log(listeOrdonnee);
+});
